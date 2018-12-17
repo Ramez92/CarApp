b2be43eaf6fc7156d177bc4ce30b6ca79f817452
R@mez <ramez.abu.ahmd@gmail.com>
2018-12-17 20:25:23 +0200

    static content added (47)

--- carapp/src/main/java/com/example/carapp/CarappApplication.java
+++ carapp/src/main/java/com/example/carapp/CarappApplication.java
@@ -1,13 +1,20 @@
 package com.example.carapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CarappApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CarappApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarappApplication.class, args);
+	    logger.info("Hello Spring Boot");
+
+
 	}
 
 }
